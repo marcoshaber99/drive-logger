d6cf60c7cb09fca726c68648f376afa63e5d2d30
Marco Haber <marcoshaber99@gmail.com>
2024-10-03 14:15:56 +0300

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-700">
+      <Button>Click me</Button>
+    </div>
+  );
+}
